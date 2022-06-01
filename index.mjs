f00f1a7c8b0caa6b19a035fd63611b6bff5ebabf
Pulak Malhotra <pulak.malhotra@students.iiit.ac.in>
2022-06-01 18:41:26 +0530

    Support for custom function

--- index.mjs
+++ index.mjs
@@ -10,71 +10,75 @@ import { DependencyResolver } from "jest-resolve-dependencies";
 import fs, { read } from "fs";
 import DependencyResolverFactory from "./dependencyResolverFactory.js";
 import cliProgress from "cli-progress";
-// import * as parser from "@babel/parser";
 import _traverse from "@babel/traverse";
 
-
-function processor(strings, code, filename) {
-  // console.log(strings, code)
-  // let ast;
-  // try {
-  // ast = parser.parse(code, {
-  //   plugins: ["@babel/plugin-syntax-jsx", "@babel/plugin-transform-typescript"],
-  //   sourceType: "unambiguous",
-  // });
-  // } catch (err) {
-  //   console.log("File:", filename)
-  //   console.log(err)
-  //   return
-  // } 
-  let x = babel.transformSync(code, {
-    filename: filename,
-    presets: ["@babel/preset-typescript"],
-    code: false,
-    ast: true
-  },
-  );
-  let ast = x.ast
-  const traverse = _traverse.default;
-  traverse(ast, {
-    CallExpression: function (path) {
-      const node = path.node;
-      if (
-        node.arguments &&
-        node.arguments[0].type === "StringLiteral" &&
-        node.callee &&
-        node.callee.name === "getString"
-      ) {
-        strings.push(path.node.arguments[0].value);
-      }
-    },
-  });
+function processor(extractorFunctionName, code, filename) {
+  let stringsFound = [];
+  try {
+    const { ast: ast } = babel.transformSync(code, {
+      filename: filename,
+      presets: [
+        "@babel/preset-typescript",
+        "@babel/preset-react",
+        "@babel/preset-flow",
+      ],
+      plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
+      code: false,
+      ast: true,
+    });
+    const traverse = _traverse.default;
+    traverse(ast, {
+      CallExpression: function (path) {
+        const node = path.node;
+        if (
+          node.arguments &&
+          node.arguments.length > 0 &&
+          node.arguments[0].type === "StringLiteral" &&
+          node.callee &&
+          node.callee.name === extractorFunctionName
+        ) {
+          stringsFound.push(path.node.arguments[0].value);
+        }
+      },
+    });
+  } catch (err) {
+    return false;
+  }
+  return stringsFound;
 }
 
 async function processFiles(allFiles, processor) {
   const allCodes = [];
+  const errorFiles = [];
+  const stringsFound = [];
   let done = 0;
-  console.log(chalk.bold(`❯ Reading ${chalk.blue(allFiles.length)} files`));
   const bar1 = new cliProgress.SingleBar(
     {},
     cliProgress.Presets.shades_classic
   );
-  // bar1.start(allFiles.length, 0);
+  bar1.start(allFiles.length, 0);
   await Promise.all(
     Array.from(allFiles).map(async (file) => {
       const code = await fs.promises.readFile(file, "utf8");
       allCodes.push(code);
-      processor(code, file);
-      //   bar1.update(++done);
+      const ret = processor(code, file);
+      if (!ret) {
+        errorFiles.push(file);
+      } else {
+        stringsFound.push(...ret);
+      }
+      bar1.update(++done);
     })
   ).catch(console.log);
-  // bar1.stop();
+  bar1.stop();
+  return { errorFiles, stringsFound };
 }
 
 async function getStringsToTranslate({
-  entryPoint: entryPoint,
-  rootDir: rootDir,
-  extensions: extensions,
+  entryPoint,
+  rootDir,
+  extensions,
+  extractorFunctionName,
 }) {
   const root = resolve(process.cwd(), rootDir);
   console.log(
@@ -82,7 +86,7 @@ async function getStringsToTranslate({
   );
 
   const hasteMapOptions = {
-    extensions: ["js", "ts", "tsx", "jsx"],
+    extensions,
     name: "jest-bundler",
     platforms: [],
     rootDir: root,
@@ -94,20 +98,17 @@ async function getStringsToTranslate({
   await hasteMap.setupCachePath(hasteMapOptions);
   const { hasteFS, moduleMap } = await hasteMap.build();
 
-  entryPoint = entryPoint.map((entryPoint) =>
-    resolve(process.cwd(), entryPoint)
-  );
-
-  for (let point of entryPoint) {
-    if (!hasteFS.exists(point)) {
-      console.log(
+  const entryPointAbsolute = entryPoint.map((entryPoint) => {
+    let absolutePath = resolve(process.cwd(), entryPoint);
+    if (!hasteFS.exists(absolutePath)) {
+      throw new Error(
         chalk.red(
-          "entryPoint does not exist. Please provide a path to valid file"
+          `${entryPoint} does not exist. Please provide a path to valid file`
         )
       );
-      return;
     }
-  }
+    return absolutePath;
+  });
 
   console.log(chalk.bold(`❯ Resolving dependencies recursively`));
 
@@ -116,16 +117,13 @@ async function getStringsToTranslate({
     rootDir: root,
   };
 
-  // const dependencyResolver = new DependencyResolver(resolver, hasteFS)
-  // console.log(dependencyResolver.resolve(entryPoint))
-
   const depFactory = new DependencyResolverFactory(
     extensions,
     moduleMap,
     resolverOpts,
     hasteFS
   );
-  const queue = entryPoint;
+  const queue = entryPointAbsolute;
   const allFiles = new Set();
 
   while (queue.length) {
@@ -136,22 +134,42 @@ async function getStringsToTranslate({
     }
 
     allFiles.add(module);
-    // console.log(module, hasteFS.getDependencies(module))
-    // console.log("resolving dependencies of", module);
-    // console.log(module, dependencyResolver.resolve(module))
     let dependencies = depFactory.multiResolve(module);
-    // console.log(module, dependencies)
     queue.push(...dependencies);
   }
 
   console.log(chalk.bold(`❯ Found ${chalk.blue(allFiles.size)} files`));
-  const stringsFound = [];
-  await processFiles(Array.from(allFiles), (code, filename) => {
-    processor(stringsFound, code, filename);
-  });
-  
-  console.log(chalk.bold(`❯ Found ${stringsFound.length} strings in the files`));
+  const { errorFiles, stringsFound } = await processFiles(
+    Array.from(allFiles),
+    (code, filename) => {
+      return processor(extractorFunctionName, code, filename);
+    }
+  );
+  console.log(
+    chalk.bold(`❯ ${chalk.blue(errorFiles.length)} files could not be parsed`)
+  );
+  errorFiles.length && console.log(errorFiles);
+  const stringsSet = new Set(stringsFound);
+  console.log(
+    chalk.bold(
+      `❯ Found ${chalk.blue(
+        stringsFound.length
+      )} strings out of which ${chalk.blue(stringsSet.size)} are unique`
+    )
+  );
+  stringsFound.length && console.log(new Set(stringsFound));
 }
 export default getStringsToTranslate;
 
-// getStringsToTranslate({entryPoint: ["./product/entry-point.js"], rootDir: "./product", extensions: ["ios.js", "js", "tsx", "ts"]})
+// getStringsToTranslate({
+//   entryPoint: ["./product/entry-point.js"],
+//   rootDir: "./product",
+//   extensions: ["ios.js", "js", "tsx", "ts"],
+//   extractorFunctionName: "getString"
+// });
+getStringsToTranslate({
+  entryPoint: ["../i18n/eigen/index.android.js", "../i18n/eigen/index.ios.js"],
+  rootDir: "../i18n/eigen",
+  extensions: ["ios.js", "js", "tsx", "ts"],
+  extractorFunctionName: "useFeatureFlag",
+});
